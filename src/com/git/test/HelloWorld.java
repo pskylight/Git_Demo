3a9c4aa3132b9afe5ff0e940249f523fc82b8af8
pskylight <362277458@qq.com>
2016-03-15 19:25:44 +0800

    11111111

--- src/com/git/test/HelloWorld.java
+++ src/com/git/test/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 
 		System.out.println("hello world!");
-		System.out.println("22222222222");
+		System.out.println("11111111111");
 	}
 
 }
